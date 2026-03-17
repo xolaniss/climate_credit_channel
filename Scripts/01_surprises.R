c81a0877450ecba3d66f75fabe2ac760d1d6e498
Xolani Sibande <xolaniss@gmail.com>
2026-03-17 10:35:27 +0200

    surprises from matlab to R

--- Scripts/01_surprises.R
+++ Scripts/01_surprises.R
@@ -3,31 +3,22 @@
 
 # Preliminaries -----------------------------------------------------------
 library(here)
+library(R.matlab)
 
 # Functions ---------------------------------------------------------------
 source(here("packages.R"))
 source(here("Functions", "fx_plot.R"))
 
 # Import -------------------------------------------------------------
-
-
-# Cleaning -----------------------------------------------------------------
-
-
-# Transformations --------------------------------------------------------
-
-
-# EDA ---------------------------------------------------------------
-
-
-# Graphing ---------------------------------------------------------------
-
+sa_surprises_tbl <- 
+  R.matlab::readMat(here("Data", "IRFs", "IRFs.mat")) |> 
+  as_tibble() # Find out the names and the dates of the surprises
 
 # Export ---------------------------------------------------------------
-artifacts_ <- list (
-
+artifacts_sa_surprises <- list (
+  sa_surprises_tbl = sa_surprises_tbl
 )
 
-write_rds(artifacts_, file = here("Outputs", "artifacts_.rds"))
+write_rds(artifacts_sa_surprises, file = here("Outputs", "artifacts_sa_surprises.rds"))
 
 
